8f775c91832c9aff7716b0d74ed3e39191a7dd1f
Søren Rasmussen <brokzftw@gmail.com>
2012-06-03 00:53:11 +0200

    Added new user service

---
+++ services/userService.js
@@ -0,0 +1,28 @@
+define(['framework/core', 'models/userModel'], function (core, usermodel) {
+    return core.Base.extend({
+        constructor:function () {
+            this.userModel = usermodel;
+        },
+
+        createUser:function () {
+            var user = new this.userModel();
+
+            user.username = 'soeren';
+            user.password = 'test';
+
+            user.save(function(error){
+                if (error != null)
+                    throw new Error('Something went wrong..', error);
+            });
+        },
+
+        getUser:function (options, callback) {
+            this.userModel.findOne({username:options.username}, function (error, user) {
+                if (error)
+                    callback(null);
+
+                callback(user);
+            });
+        }
+    });
+});
