b0583a68e25fa6655590847e843dcf04aedf7a35
Søren Rasmussen <brokzftw@gmail.com>
2012-06-03 01:57:55 +0200

    Added new draft property on post model

--- models/postModel.js
+++ models/postModel.js
@@ -2,7 +2,8 @@ define(['framework/core'], function(core){
     var postSchema = new core.mongoose.Schema({
         title: String,
         content: String,
-        date: Date
+        date: Date,
+        draft: Boolean
     });
 
     var postModel = core.mongoose.model('Post', postSchema);
