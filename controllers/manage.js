084ded1db2fa1faa68da6fafaad0e903d7f0d5f1
Søren Rasmussen <brokzftw@gmail.com>
2012-06-03 00:53:21 +0200

    Added new manage controller

---
+++ controllers/manage.js
@@ -0,0 +1,24 @@
+define(['framework/core', 'services/blogService', 'services/userService'], function (core, blogService, userService) {
+    var server = core.server;
+    blogService = new blogService();
+    userService = new userService();
+
+    return core.Base.extend({
+        constructor:function () {
+            server.get('/manage', function(request, response){
+                console.log('request session login: ', request.session.login);
+
+                response.render('manage', {
+                    title: 'manage - srn.io',
+                    user: request.session.login
+                });
+            });
+
+            server.get('/manage/logout', function (request, response) {
+                request.session.login = null;
+
+                response.redirect('/');
+            });
+        }
+    });
+});
