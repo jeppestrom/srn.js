d547580baf4a93081536330a7af013aae47f31b2
Søren Rasmussen <brokzftw@gmail.com>
2012-06-06 22:38:42 +0200

    Added new rss feed (/feed.rss)

--- controllers/index.js
+++ controllers/index.js
@@ -13,6 +13,31 @@ define(['framework/core', 'services/blogService', 'services/userService'], funct
                     });
                 });
             });
+
+            server.get('/feed.rss', function (request, response) {
+                var rss = require('rss');
+                var feed = new rss({
+                    title: 'srn.io (Soren Brokaer)',
+                    description: 'Software-developer  @spiir, loves .net, ruby, nodejs and knockoutjs. 18 years old.',
+                    feed_url: 'http://srn.io/feed.rss',
+                    site_url: 'http://srn.io',
+                    author: 'Soren Brokaer'
+                });
+
+                blogService.getLivePosts(function (posts) {
+                    posts.forEach(function (post) {
+                        feed.item({
+                            title: post.title,
+                            description: post.content.substring(0, 50),
+                            url: 'http://srn.io/', // TODO: post.url
+                            date: post.date
+                        });
+                    });
+
+                    response.header("Content-Type", "application/rss+xml");
+                    response.end(feed.xml());
+                });
+            });
         }
     });
 });
