74c17b22fbb73aa9bcce899faaf9279cf6d568bf
Søren Rasmussen <brokzftw@gmail.com>
2012-06-07 03:42:42 +0200

    Added routing for handling a single blog post

--- controllers/post.js
+++ controllers/post.js
@@ -4,6 +4,21 @@ define(['framework/core', 'services/blogService'], function (core, blogService) 
 
     return core.Base.extend({
         constructor:function () {
+            server.get('/favicon.ico', function (request, response) { response.end(); }); // quick workaround, needs a proper fix
+
+            server.get('/:url', function (request, response) {
+                var posts = [];
+
+                blogService.getPostByUrl({url: request.params.url}, function (post) {
+                    posts.push(post);
+
+                    response.render('index',{
+                        title: post.title + ' - srn.io',
+                        posts: posts
+                    });
+                });
+            });
+
             server.get('/archive', function(request, response){
                 blogService.getLivePosts(function (posts) {
                     response.render('archive', {
